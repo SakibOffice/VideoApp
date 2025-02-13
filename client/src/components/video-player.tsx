6832765b241c0fb40c17ede0e1fb30c0b18b6a04
SayemHossain <8668226-SayemHossain@users.noreply.replit.com>
2025-02-13 16:51:10 +0000

    Agent query: Could you check if you can now see the video and use fullscreen functionality?
    Improve Video Player styling and controls
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/ce188ce6-16f0-4492-ad0c-fc79ebbfac79/f9697223-3fad-4103-8d55-f949594d3ea4.jpg

--- client/src/components/video-player.tsx
+++ client/src/components/video-player.tsx
@@ -8,21 +8,37 @@ interface VideoPlayerProps {
 export function VideoPlayer({ url }: VideoPlayerProps) {
   return (
     <Card className="overflow-hidden w-full">
-      <div className="relative aspect-video">
+      <div 
+        className="relative aspect-video bg-black"
+        style={{ minHeight: '240px' }}
+      >
         <ReactPlayer
           url={url}
           width="100%"
           height="100%"
-          controls
-          playsinline
+          controls={true}
+          playing={false}
+          playsinline={true}
+          pip={false}
           config={{
             file: {
               attributes: {
-                controlsList: "nodownload",
-                style: { width: '100%', height: '100%' }
+                style: {
+                  width: '100%',
+                  height: '100%',
+                  objectFit: 'contain'
+                },
+                controlsList: 'nodownload',
               },
-              forceVideo: true
+              forceVideo: true,
+              forceHLS: false,
+              forceDASH: false
             }
+          }}
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0
           }}
         />
       </div>
